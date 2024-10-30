6dc7d360ea1526c0e2c81ee9c3ca4f28bfba8ee2
dylan-yoon <d.dylany21@gmail.com>
2024-10-31 08:26:06 +0900

    Consuming a RESTful Web Service

--- webservice/src/main/java/com/baem/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/baem/webservice/WebserviceApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebserviceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
 	}
-
 }
